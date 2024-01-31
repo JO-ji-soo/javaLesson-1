839d1b35aaae1c68f9f5d5583f9d967a8c9a2a15
Miryeoen <derange63@gmail.com>
2024-01-31 09:56:18 +0900

    240130

--- jdbcEx/src/jdbc/day2/CartApp_2.java
+++ jdbcEx/src/jdbc/day2/CartApp_2.java
@@ -1,83 +1,120 @@
 package jdbc.day2;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import project.dao.TblBuyDao;
 import project.dao.TblProductDao;
+import project.vo.BuyVo;
 import project.vo.CustomerBuyVo;
 import project.vo.ProductVo;
 
-//static 이 많아지면 상속의 특징을 사용할 수 없습니다. 멀티 스레드 환경에 적합하지 않습니다.
-//    -> 여기서는 테스트를 위해 static 사용 중입니다.
+//static이 많아지면 상속의 특징을 사용할 수 없다. 멀티스레드 환경애도 부적합
+//  =>여기서는 테스트를 위해 static 사용
 public class CartApp_2 {
 
     public static void showMenu() {
         System.out.println(".".repeat(50));
-        System.out.println("[C] 카테고리별 상품 조회     [P] 상품명 검색");
+        System.out.println("[C] 카테고리별 상품 조회     [P] 상품명 검색  [M]나의 구매내역");
         System.out.println("[B] 구매하기   [D] 구매 취소   [Q] 구매 수량 변경  [X] 구매 종료");
         System.out.println(".".repeat(50));
     }
 
-    //상품 목록을 선택한 카테고리에 대해 보여주기 (구매할 상품 조회)
-    //또는 상품명으로 검색 (구매할 상품 조회)
-    //또는 입력한 아이디로 구매한 구매내역 보여주기 (구매수량 변경 또는 구매 취소 buy_idx 조회)
-
+    // 상품 목록을 선택한 카테고리에 대해 보여주기 (구매할 상품 조회)
+    // 또는 상품명으로 검색 (구매할 상품 조회)
+    // 또는 입력한 아이디로 구매한 구매내역 보여주기 (구매수량 변경 또는 구매 취소 buy_idx 조회)
     public static void main(String[] args) {
-        CartApp_2 app = new CartApp_2();
-        TblBuyDao dao = new TblBuyDao();
+        TblBuyDao buyDao = new TblBuyDao();
         TblProductDao productDao = new TblProductDao();
         System.out.println("구매할 사용자 간편 로그인 필요합니다.");
         System.out.print("아이디 입력 __");
-        String customerId = System.console().readLine();
-        System.out.println(customerId+"님 환영합니다.♡");
-        boolean run = true;
+        String customerid = System.console().readLine();
+        System.out.println(String.format("%s님 환영합니다.", customerid));
 
-        while (run) {       //메뉴 선택 반복
+        boolean run = true;
+        while (run) { // 메뉴 선택 반복
             showMenu();
-            System.out.print("선택>>>>");
-            //int select = Integr.parseInt(System.console().readLine());
+            System.out.print("선택 >>> ");
             String select = System.console().readLine();
             switch (select) {
-                case "M","m":   //[1] 장바구니 담기 - buy 테이블에 insert (1행)
-                    List<CustomerBuyVo> result = dao.selectCustomerBuyList(customerId);
-                    for(CustomerBuyVo vo : result)
+                case "M": // 나의 구매내역
+                    List<CustomerBuyVo> result = buyDao.selectCustomerBuyList(customerid);
+                    for (CustomerBuyVo vo : result)
                         System.out.println(vo);
                     break;
-                case "C","c":
-                    System.out.println("카테고리: A1-과일 A2-수입과일 B1-인스턴스 B2-선물세트 C1-과자류");
-                    System.out.println("카테고리 입력____");
+                case "C":
+                    System.out.println("카터고리 : A1-과일 A2-수입과일 B1-인스턴스 B2-선물세트 C1-과자류");
+                    System.out.print("카테고리 입력 > ");
                     String category = System.console().readLine();
-                    List<ProductVo> productList = productDao.selectByCategory(category);
-                    for( ProductVo vo : productList)
-                                System.out.println(vo);
-                    break;     
-                case "P","p":
-                    System.out.print("상품명 검색어 입력___");
-                    String pname = System.console().readLine();
-                    productList = productDao.selectByPname(pname);
-                    for( ProductVo vo : productList)
-                                System.out.println(vo);
-                    break;
-                case "B","b":
-                    System.out.print("구매하기");
-                    System.out.print("상품 목록을 선택하세요:______");
-                    productList = productDao.getAllProducts();
-                    for (ProductVo vo : productList) 
+                    List<ProductVo> productlist = productDao.selectByCategory(category);
+                    for (ProductVo vo : productlist)
                         System.out.println(vo);
-                    break; 
-                case "D","d":
+                    break;
+                case "P":
+                    System.out.print("상품명 검색어 입력 > ");
+                    String pname = System.console().readLine();
+                    productlist = productDao.selectByPname(pname);
+                    for (ProductVo vo : productlist)
+                        System.out.println(vo);
+                    break;
+                case "B":
+                    System.out.print(" 상품코드를 입력하세요. __ ");
+                    String pcode = System.console().readLine();
+
+                    System.out.print(" 수량을 입력하세요. __ ");
+                    int quantity = Integer.parseInt(System.console().readLine());
                     
 
-                case "X","x":
-                    run=false;
-                    break;    
+                    BuyVo vo = new BuyVo(customerid, pcode, quantity, quantity, null);
+
+                    if(buyDao.buy(vo) ==1)
+                        System.out.println("상품을 담았습니다.");
+                    else 
+                        System.out.println("상품코드 또는 고객아이디 오류입니다");
+                    break;
+                case "D":
+                    System.out.print("취소할 구매 번호를 입력하세요. __ ");
+                    int buy_idx = Integer.parseInt(System.console().readLine());
+
+                    if (buyDao.delete(buy_idx)==1)
+                        System.out.println("정상적으로 최소되었습니다");
+                    else 
+                        System.out.println("없는 구매번호 입니다");
+                    break;
+
+                case "Q":
+                    System.out.print(" 수정할 구매 번호를 입력하세요. __ ");
+                    buy_idx = Integer.parseInt(System.console().readLine());
+
+                    System.out.print(" 상품코드를 입력하세요. __ ");
+                    pcode = System.console().readLine();
+
+                    System.out.print(" 변경할 수량을 입력하세요. __ ");
+                    quantity = Integer.parseInt(System.console().readLine());
+
+                    //map를 사용해보기
+                    Map<String, Integer> arg = new HashMap<>();
+                    arg.put("buy_idx", buy_idx);
+                    arg.put("quantity", quantity);
+                    
+                    if(buyDao.modify(arg) ==1)
+                        System.out.println("수정완료 되었습니다");
+                    else 
+                        System.out.println("없는 구매번호 입니다");
+
+                    // vo = new BuyVo(buy_idx, customerid, pcode, quantity, null);
+                    // buyDao.modify(vo);
+                    break;
+                case "X":
+                    return;
                 default:
                     break;
             }
         }
 
     }
-}   // tbl_buy 테이블을 대상을 insert, update, delete 할수 있는 dao 클래스 TblBuyDao.java 
-    //         테이블 컬럼과 1:1 대응되는 BuyVo.java  
-    //         테이블 PK 컬럼은 buy_idx -> update,delete 의 조건 컬럼입니다.     
-    //         insert 에서 시퀀스는 sql 실행할 때와 동일하게 사용합니다.      
+} // tbl_buy 테이블을 대상을 insert, update, delete 할수 있는 dao 클래스 TblBuyDao.java
+  // 테이블 컬럼과 1:1 대응되는 BuyVo.java
+  // 테이블 PK 컬럼은 buy_idx -> update,delete 의 조건 컬럼입니다.
+  // insert 에서 시퀀스는 sql 실행할 때와 동일하게 사용합니다.
--- jdbcEx/src/project/dao/TblBuyDao.java
+++ jdbcEx/src/project/dao/TblBuyDao.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import project.vo.BuyVo;
 import project.vo.CustomerBuyVo;
@@ -17,106 +18,133 @@ public class TblBuyDao {
     private static final String PASSWORD = "1234";
 
     private Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
+    //tbl_buy 테이블을 대상으로 insert, update, delete 할 수 있는 dao 클래스 만들기 -TblBuyDao.java
+    //테이블 컬럼과 1:1 대응되는 BuyVo.java 만들기 
+    //테이블의 pk 컬럼은 BUY_IDX :  update, delete 할때 조건 컬럼으로 사용됨 (행을 식별) -> 모든 조건은 pk
 
-    //구매하기
-    public void Buy(BuyVo vo){
-        String sql="insert into tbl_buy(buy_idx, customId,pcode,quantity,buy_date)" + 
-                    //insert into tbl_buy \r\n
-        "values (Buy_pk_seq.nextval, ?, ?, ?, sysdate)";
-        try( //auto close
-            Connection connection = getConnection();  
-            PreparedStatement pstmt = connection.prepareStatement(sql);  
-        ){ 
-            pstmt.setString(1, vo.getCustomId());
-            pstmt.setString(2, vo.getPcode());
-            pstmt.setInt(3, vo.getQuantity());
+    //장바구니 담기  - insert
+    public void buy(BuyVo bv, String userId){
+        String sql = "insert into tbl_buy(BUY_IDX, CUSTOMID, PCODE, QUANTITY, BUY_DATE)" +
+        "VALUES(buy_pk_seq.nextval, ?, ?, ? , sysdate)";
 
-            pstmt.executeUpdate();                  
-        }catch(SQLException e){
-            System.out.println("Buy 실행 예외 발생 :"+e.getMessage());
-        }
-    }
-
-    //구매 수량 수정 - PK는 행 식별합니다. 특정 행을 수정하려면 where조건 컬럼은 buy_idx
-    public void modify(BuyVo vo){
-        String sql="update tbl_buy set quantity = ? where buy_idx = ?";
-    
-        try(
-            Connection connection = getConnection();
+        try (Connection connection = getConnection();       //auto close
             PreparedStatement pstmt = connection.prepareStatement(sql);
-        ){
-            pstmt.setInt(1, vo.getQuantity());
-            pstmt.setInt(2, vo.getBuy_idx());
-
-            pstmt.executeUpdate(); //실행
-        }catch(SQLException e){
-            System.out.println("Buy 실행 예외 발생 :"+e.getMessage());
-        }
-    
-    }
-
-    //구매 수량 변경
-    public void update(BuyVo vo){
-        String sql="update tbl_buy set quantity = ? where buy_idx = ?";
-    
-        try(
-            Connection connection = getConnection();
-            PreparedStatement pstmt = connection.prepareStatement(sql);
-        ){
-            pstmt.setInt(1, vo.getQuantity());
-            pstmt.setInt(2, vo.getBuy_idx());
-
-            pstmt.executeUpdate(); //실행
-        }catch(SQLException e){
-                System.out.println("Buy 실행 예외 발생 :"+e.getMessage());
-        }
-    }
-
-    //구매 취소 - PK는 행 식별합니다. 특정 행을 삭제하려면 where조건 컬럼은 buy_idx
-    public void delect(BuyVo vo){
-        String sql="delect from tbl_buy where buy_idx = ?"; 
-        try( //auto close
-            Connection connection = getConnection();  
-            PreparedStatement pstmt = connection.prepareStatement(sql);  
-        ){ 
-            pstmt.setInt(1, vo.getBuy_idx());
-
-            pstmt.executeUpdate();                  
-        }catch(SQLException e){
-            System.out.println("Buy 실행 예외 발생 :"+e.getMessage());
-        }
-    }
-
-    //mypage 기능의 메소드.
-    public List<CustomerBuyVo> selectCustomerBuyList(String category) {
-        List<CustomerBuyVo> list = new ArrayList<>();
-        String sql = "SELECT BUY_IDX, tb.PCODE, PNAME, PRICE, QUANTITY, BUY_DATE \r\n"
-                    +"FROM TBL_BUY tb\r\n"
-                    +"JOIN TBL_PRODUCT tp\r\n"
-                    +"ON tb.PCODE = tp.PCODE\r\n"
-                    +"WHERE tb.CUSTOMID = ?\r\n" 
-                    +"ORDER BY BUY_DATE DESC";
-        try (
-            Connection connection = getConnection();
-            PreparedStatement psmt = connection.prepareStatement(sql);
-        ) {
-            psmt.setString(1, category); 
-            ResultSet rs = psmt.executeQuery();
-            
-            while (rs.next()) { //조회 결과는 N행 가능성 예측
-                list.add(new CustomerBuyVo(
-                        rs.getInt(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getInt(4),
-                        rs.getInt(5),
-                        rs.getTimestamp(6)));
-            }rs.close();
+            )
+            {   //2. 매개변수 바인딩 (매개변수 타입에 맞는 메소드를 실행)
+                pstmt.setString(1, userId);
+                pstmt.setString(2, bv.getPcode());
+                pstmt.setInt(3, bv.getQuantity());
+                pstmt.executeUpdate();
+                
         } catch (SQLException e) {
-            System.out.println("selectByCategory 실행 예외 발생: " + e.getMessage());
+            System.out.println("구매 실행 예외 발생 : " + e.getMessage());
         }
-        return list;
+        //customid 와 pcode는 참조테이블에 존재하는 값으로 안하면 무결성 위반오류
+    }
+
+
+    //executeUpdate 메소드의 result 반영
+    public int buy(BuyVo bv){
+        String sql = "insert into tbl_buy(BUY_IDX, CUSTOMID, PCODE, QUANTITY, BUY_DATE)" +
+        "VALUES(buy_pk_seq.nextval, ?, ?, ? , sysdate)";
+        int result=0;
+
+        try (Connection connection = getConnection();       //auto close
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            )
+            {   //2. 매개변수 바인딩 (매개변수 타입에 맞는 메소드를 실행)
+                pstmt.setString(2, bv.getPcode());
+                pstmt.setInt(3, bv.getQuantity());
+                result = pstmt.executeUpdate();
+                
+        } catch (SQLException e) {
+            System.out.println("구매 실행 예외 발생 : " + e.getMessage());
+        }return result;
+        //customid 와 pcode는 참조테이블에 존재하는 값으로 안하면 무결성 위반오류
+    }
+
+    //executeUpdate 메소드의 result 반영 - 구매 취소 
+    public int delete(int buyidx){
+        int result=0;
+        String sql = "delete from tbl_buy where BUY_IDX =?";
+        try (Connection connection = getConnection();      
+            PreparedStatement pstmt = connection.prepareStatement(sql);)
+            {pstmt.setInt(1, buyidx);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("구매취소 실행 예외 발생 : " + e.getMessage());
+        }return result;
+    }
+
+    //[2] 구매 취소   
+    public void delete(BuyVo bv){
+        String sql = "delete from tbl_buy where BUY_IDX =?";
+        try (Connection connection = getConnection();      
+            PreparedStatement pstmt = connection.prepareStatement(sql);)
+            {pstmt.setInt(1, bv.getBuy_idx());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("구매취소 실행 예외 발생 : " + e.getMessage());
+    }
+}
+    //executeUpdate 메소드는 insert,update, delete가 정상실행(반영된 행이 있으면)되면 1을 리턴
+    //특히 update나 delete는 조건에 맞는 행이 없어 반영되지 않으면 0을 리턴
+    //[3] 구매 수량 변경 -pk로 행을 식별 
+    public void modify(BuyVo bv,int changeidx, int number){
+        String sql ="UPDATE TBL_BUY SET QUANTITY = ? WHERE BUY_IDX = ?";
+        try (Connection connection = getConnection();      
+            PreparedStatement pstmt = connection.prepareStatement(sql);){
+            pstmt.setInt(1, number);
+            pstmt.setInt(2, bv.getBuy_idx());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("구매수량변경 실행 예외 발생 : " + e.getMessage());
+        }
+    }
+    //MAP 으로 넣기
+    public void modify(Map<String ,Integer> arg){
+        String sql ="UPDATE TBL_BUY SET QUANTITY = ? WHERE BUY_IDX = ?";
+
+        try (Connection connection = getConnection();      
+            PreparedStatement pstmt = connection.prepareStatement(sql);){
+            pstmt.setInt(1, arg.get("quantity"));
+            pstmt.setInt(2, arg.get("buyidx"));
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("구매수량변경 실행 예외 발생 : " + e.getMessage());
+        }
+    }
+    //buyidx에 없는 값이면 오류는 아니고 update반영한 행의 개수가 0이 됨
+
+
+
+
+    //mypage 기능의 메소드
+    public List<CustomerBuyVo> selectCustomerBuyList(String costumerId){
+        List<CustomerBuyVo> list = new ArrayList<>();
+        String sql = "SELECT BUY_IDX, TB.PCODE, PNAME,PRICE,QUANTITY ,BUY_DATE \r\n" + 
+            "FROM TBL_BUY tb \r\n" + 
+            "JOIN TBL_PRODUCT tp \r\n" + 
+            "ON TB.PCODE = TP.PCODE \r\n" + 
+            "WHERE TB.CUSTOMID =? \r\n"+
+            "ORDER by BUY_DATE desc";
+            try (Connection connection = getConnection();      
+                PreparedStatement pstmt = connection.prepareStatement(sql);)    
+                {pstmt.setString(1, costumerId);
+                 ResultSet rs = pstmt.executeQuery();
+                //pstmt.executeQuery();//executeUpdate() write용 executeQuery() read용- 잘못됨
+                while (rs.next()) {
+                list.add(new CustomerBuyVo(rs.getInt("BUY_IDX"),
+                                            rs.getString("PCODE"), 
+                                            rs.getString("PNAME"), 
+                                            rs.getInt("PRICE"),
+                                            rs.getInt("QUANTITY"),
+                                            rs.getTimestamp("BUY_DATE")));
+                }
+            } catch (SQLException e) {
+            System.out.println("고객별 구매내역 실행 예외 발생 : " + e.getMessage());
+            }return list;
     }
 }
